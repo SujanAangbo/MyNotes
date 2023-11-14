af5f3a8a89fefc456525fc2e6d47a81fa93fa830
Sujan <sujanaangbo100@gmail.com>
2023-11-14 18:59:41 +0545

    Optimized button for dark mode too

--- bla.java
+++ bla.java
@@ -1,5 +1,7 @@
 public class bla {
     public static void main(String[] args) {
+        
+        System.out.println("Make the button works on dark mode too.");
         System.out.println("Added add new notes to home page.");
         System.out.println("Fixex bug");
     }
