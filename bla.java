9be729a3702646913fa287b37b5a133216200370
Sujan <sujanaangbo100@gmail.com>
2023-11-14 18:49:52 +0545

    added new notes button to the home page

--- bla.java
+++ bla.java
@@ -1,5 +1,6 @@
 public class bla {
     public static void main(String[] args) {
+        System.out.println("Added add new notes to home page.");
         System.out.println("Fixex bug");
     }
 }
